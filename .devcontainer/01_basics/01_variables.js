8a1ae7583a016d9bea58ee68d641898277b1556f
2002srishti <127188360+2002srishti@users.noreply.github.com>
2024-07-18 15:38:31 +0000

    container config done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=13225;
+let accountEmail="srishti@google.com"
+var accountPassword="`213434"
+accountCity="Kolkata"
+accountEmail="ss@ss.com"
+accountPassword="3456"
+
+//accountId=2
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
